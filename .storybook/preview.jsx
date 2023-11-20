cbea24f0ad4f12aec8f68fac2a57c2491e54cd7f
Rizwan Kukad <rizvankukad@gmail.com>
2023-11-20 16:35:38 +0530

    second commit

--- .storybook/preview.jsx
+++ .storybook/preview.jsx
@@ -1,5 +1,17 @@
 /** @type { import('@storybook/react').Preview } */
+
+import { Global } from '@emotion/react';
+import { GlobalStyle } from '../src/shared/global';
+
 const preview = {
+  decorators: [
+    (Story) => (
+      <>
+        <Global styles={GlobalStyle} />
+        <Story />
+      </>
+    ),
+  ],
   parameters: {
     actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
